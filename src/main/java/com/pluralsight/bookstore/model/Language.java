af61e642d66d22fada92763d8026e212382b8bdc
Francisco Zitha <francisco.zitha@standardbank.co.mz>
2024-10-13 21:27:37 +0200

    initial commit with jpa configured for H2

---
+++ src/main/java/com/pluralsight/bookstore/model/Language.java
@@ -0,0 +1,9 @@
+package com.pluralsight.bookstore.model;
+
+/**
+ * @author Claucio Zitha
+ * A246675
+ */
+public enum Language {
+    ENGLISH, FRENCH, SPANISH, PORTUGUESE, ITALIAN, GERMAN, DEUSTSH, RUSSIAN
+}
